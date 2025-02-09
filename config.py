ef0118c92362047277f4b187b9f7127a7023146c
ductrungbunnt <caoductrung1932010@gmail.com>
2025-02-09 18:43:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://admin:admin@cluster0.qmg6y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://admin:admin0@cluster0.qmg6y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 config = Config()
